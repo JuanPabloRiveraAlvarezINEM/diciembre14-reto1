b91f78c166747ffb3546e62b07b93190e895d056
JuanPabloRiveraAlvarezINEM <juanpariveraalvarez@gmail.com>
2020-12-14 17:30:52 -0500

    reto2

--- app.js
+++
@@ -1,9 +0,0 @@
-let caracter = 'x'
-let caja = document.getElementById("arbol");
-let num = 1;
-for(let i = 0; i<50; i++){
-    for(let j = 0; j<num;j++){
-        document.getElementById("arbol").innerHTML = caracter;
-    }
-    num = num +2
-}
